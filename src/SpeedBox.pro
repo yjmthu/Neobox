eabb611aff08f9f7f82b88ac8631bf69d780aee2
Wiley2046 <73242138+yjmthu@users.noreply.github.com>
2021-08-07 09:31:42 +0800

    Delete src directory

--- src/SpeedBox.pro
+++
@@ -1,4 +0,0 @@
-TEMPLATE = subdirs
-
-SUBDIRS =   main \
-            reg_plugin
--- src/main/scripts/SetWallPaper.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-script='var allDesktops = desktops();print (allDesktops);for (i=0;i<allDesktops.length;i++) {d = allDesktops[i];d.wallpaperPlugin = "org.kde.image";d.currentConfigGroup = Array("Wallpaper", "org.kde.image", "General");d.writeConfig("Image", "file://'$1'")}'
-export DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/1000/bus"
-qdbus org.kde.plasmashell /PlasmaShell org.kde.PlasmaShell.evaluateScript "$script"
-exit 0
