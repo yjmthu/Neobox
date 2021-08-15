daa2b4176981e8d52ada6962687f559da397e018
Wiley2046 <73242138+yjmthu@users.noreply.github.com>
2021-08-15 13:41:28 +0800

    Add files via upload

--- update/update.cpp
+++ update/update.cpp
@@ -18,8 +18,8 @@ BOOL PathFileExists(LPCSTR pszPath)
     return ret;
 }
 
-typedef BOOL(WINAPI*pfnSHGetSpecialFolderPath)(_Reserved_ HWND hwnd, _Out_writes_(MAX_PATH) LPSTR pszPath, _In_ int csidl, _In_ BOOL fCreate);
-BOOL GetSpecialFolderPath(_Reserved_ HWND hwnd, _Out_writes_(MAX_PATH) LPSTR pszPath, _In_ int csidl, _In_ BOOL fCreate)
+typedef BOOL(WINAPI* pfnSHGetSpecialFolderPath)(_Reserved_ HWND hwnd, _Out_writes_(MAX_PATH) LPSTR pszPath, _In_ int csidl, _In_ BOOL fCreate);
+BOOL GetSpecialFolderPath(HWND hwnd,LPSTR pszPath,int csidl,BOOL fCreate)
 {
     pfnSHGetSpecialFolderPath pGetSpecialFolderPath = NULL;
     BOOL ret = FALSE;
@@ -33,20 +33,19 @@ BOOL GetSpecialFolderPath(_Reserved_ HWND hwnd, _Out_writes_(MAX_PATH) LPSTR psz
         }
         FreeLibrary(hShell);
     }
+    return ret;
 }
 
 int main()
 {
-    cout << "请稍等...\n";
-    Sleep(1000); char str[MAX_PATH] = { 0 };
+    Sleep(600); char str[MAX_PATH] = { 0 };
     GetSpecialFolderPath(NULL, str, CSIDL_LOCAL_APPDATA, FALSE);
     string s = str;
     s += "\\SpeedBox\\SpeedBox.exe";
     if (PathFileExists(s.c_str()))
     {
-        cout << "正在复制文件...\n";
         memset(str, 0, sizeof(char) * MAX_PATH);
-		GetModuleFileNameA(NULL, str, MAX_PATH);
+        GetModuleFileNameA(NULL, str, MAX_PATH);
         for (short j = MAX_PATH - 1; j >= 0; --j)
         {
             if (str[j] == '\\')
@@ -62,14 +61,7 @@ int main()
             DeleteFileA(t.c_str());
         }
         MoveFileA(s.c_str(), t.c_str());
-        cout << "复制文件成功！即将启动新版本————" << endl;
         ShellExecuteA(NULL, NULL, t.c_str(), NULL, NULL, SW_SHOWNORMAL);
-        cout << "Speed-Box 升级成功！" << endl;
     }
-    else
-    {
-        cout << "未成功下载新版本！\n";
-    }
-    Sleep(1000);
     return 0;
 }
