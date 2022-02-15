1c1ad1c27185c92c42d05edfaba53a0ac395af0a
Wiley2046 <73242138+yjmthu@users.noreply.github.com>
2022-02-15 19:27:51 +0800

    Update blankform.h

--- src/blankform.h
+++ src/blankform.h
@@ -9,19 +9,17 @@ class BlankFrom: public QWidget
 {
     Q_OBJECT
 protected:
-    void enterEvent(
 #if (QT_VERSION_CHECK(6,0,0) > QT_VERSION)
-          QEvent
+    void enterEvent(QEvent *event);           // 鼠标进入后改变图标
 #else
-          QEnterEvent
+    void enterEvent(QEnterEvent *event);      // 鼠标进入后改变图标
 #endif
-            *event);    // 鼠标进入后改变图标
-    void leaveEvent(QEvent *event);         // 鼠标移出后改变图标
+    void leaveEvent(QEvent *event);           // 鼠标移出后改变图标
 public:
-    explicit BlankFrom(QWidget* p);         // 必须指定父窗口
+    explicit BlankFrom(QWidget* p);           // 必须指定父窗口
     ~BlankFrom();
-    QPushButton *closeButton;               // 关闭按钮
-    QPushButton *minButton;                 // 最小化按钮
+    QPushButton *closeButton {nullptr};       // 关闭按钮
+    QPushButton *minButton   {nullptr};       // 最小化按钮
 };
 
 
