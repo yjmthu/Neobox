b4619177b1900047506fd5c4c84fa6b646f66b8f
Wiley2046 <yjmthu@outlook.com>
2022-11-22 23:08:02 +0800

    Update xmake.lua

--- translate/xmake.lua
+++ translate/xmake.lua
@@ -1,9 +1,9 @@
-add_requires("openssl", "libcurl")
+add_requires("libcurl")
 
 target("translate")
   set_kind("static")
   add_files("src/*.cpp")
-  add_packages("libcurl", "openssl")
+  add_packages("libcurl")
   add_deps("yjson")
 target_end()
 
