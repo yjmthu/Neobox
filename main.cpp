64fa5cbfb2a166ba7cca24ca127ee6792539bdc6
Megix Cool <yjmthu@outlook.com>
2023-08-20 19:08:52 +0800

    修正debug模式编译错误

--- main.cpp
+++ main.cpp
@@ -7,17 +7,20 @@
 int main(int argc, char* argv[]) {
   QApplication a(argc, argv);
   a.setQuitOnLastWindowClosed(false);
+#ifdef _RELEASE
   try {
     PluginMgr manager;
     manager.Exec();
-#ifndef _DEBUG
   } catch (const std::runtime_error& error) {
     auto msg = QString::fromLocal8Bit(error.what());
     QMessageBox::critical(nullptr, "Runtime Error", msg);
   } catch (const std::logic_error& error) {
     auto msg = QString::fromLocal8Bit(error.what());
     QMessageBox::critical(nullptr, "Logic Error", msg);
-#endif
   }
+#else
+  PluginMgr manager;
+  manager.Exec();
+#endif
   return 0;
 }
